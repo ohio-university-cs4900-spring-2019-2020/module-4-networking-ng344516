753ef6176c9b711c12a91d22d4b44e7efcacf9ea
Nic Gill <ng344516>
2020-04-28 01:56:03 -0400

    Initial commit

---
+++ NewModule/src/NetModule.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "NetMsg.h"
+#include "../../../include/aftr/Vector.h"
+#ifdef AFTR_CONFIG_USE_BOOST
+
+namespace Aftr {
+
+	class NetModule: public NetMsg {
+	public:
+		//NetMsgMacroDeclaration(NetModule);
+		NetModule(const Vector position = (0.0,0.0,0.0));
+		virtual ~NetModule();
+		virtual bool toStream(NetMessengerStreamBuffer& os) const;
+		virtual bool fromStream(NetMessengerStreamBuffer is);
+		virtual void onMessageArrived();
+		virtual std::string toString() const;
+	protected:
+		Vector position;
+	};
+
+} // Namespace Aftr
+
+#endif
